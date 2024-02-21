44ba148316539e7c1b0842aaaff77bb512544502
Genrtul G <genrtul@gmail.com>
2024-02-21 16:16:58 +0100

    hashfix

--- ocr_subnet/protocol.py
+++ ocr_subnet/protocol.py
@@ -27,7 +27,7 @@ def hash_tensor(emission):
     bytes = []
     for f in emission:
         bytes.extend(struct.pack("f",f))
-    return base64.decode(hashlib.sha256(b''.join(bytes)).digest())
+    return base64.encodebytes(hashlib.sha256(b''.join(bytes)).digest())
 
 class EmissionPredictorSynapse(bt.Synapse):
     pass

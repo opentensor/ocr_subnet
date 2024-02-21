a96ac389d5b7d21b9e2986fda2e2c493cca757a1
Genrtul G <genrtul@gmail.com>
2024-02-21 18:29:52 +0100

    Print emission

--- ocr_subnet/validator/reward.py
+++ ocr_subnet/validator/reward.py
@@ -62,14 +62,13 @@ def is_hash_object(obj):
 
 def reward(self, unhash, hash, emission) -> float:
     predictions = unhash
-    print(hash, hash_tensor(predictions)) #DEBUG
     if predictions is None or hash is None:
         return 0.0
     
     if hash_tensor(predictions) != hash:
         return 0.0
     
-    print("PRED:", predictions)
+    print("PRED:", emission, predictions) # DEBUG
     prediction_reward = compute_rmse(predictions, emission)
 
     #time_reward = max(1 - response.time_elapsed / self.config.neuron.timeout, 0)
